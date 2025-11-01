ee5253cb6a24e1b00e5b28401ba66c59a4f60cc6
Bunny-patel2222 <bhargavjpatel222@gmail.com>
2025-11-01 14:37:13 +0530

    Added assert validation

--- src/test/java/org/saucedemo/tests/LoginTest.java
+++ src/test/java/org/saucedemo/tests/LoginTest.java
@@ -17,6 +17,9 @@ public    String username =  ConfigReader.getProperty("Standarduser");
         LoginPage.login(username, password);
 
 
+        // Add assertions here to verify successful login
+        assert driver.getCurrentUrl().contains("inventory.html");
+
 
     }
 }
